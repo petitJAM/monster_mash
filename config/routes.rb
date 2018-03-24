6c0054389c6ed9fcca4658bf2e46b898c912da6d
Alex Petitjean <alex@tenforwardconsulting.com>
2018-03-23 19:26:40 -0500

    Update gems and add postgres

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :monsters
+
+  root to: 'monsters#index'
 end
